045ddf95d8c77a49534214021c7955f141df9392
slotekilla <rok.meskic@gmail.com>
2025-09-01 18:12:24 +0200

    Fix remaining merge conflicts in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,14 +54,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-<<<<<<< HEAD
-        <meta
-          property="fc:miniapp"
-          content='{"version":"1","imageUrl":"https://quote.meskobrand.eu/rok-mesko-share-card-1200x800.png","button":{"title":"Get Inspired","action":{"type":"launch_frame","name":"Power Quotes","url":"https://quote.meskobrand.eu","splashImageUrl":"https://quote.meskobrand.eu/splash.png","splashBackgroundColor":"#000000"}}}'
-        />
-      </head>
-      <body>{children}</body>
-=======
         {/* Farcaster Mini App embed */}
         <meta property="fc:miniapp" content={fcMiniApp} />
         <meta name="fc:miniapp" content={fcMiniApp} />
@@ -72,7 +64,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <MiniAppInit />
         <div className="rm-wrap">{children}</div>
       </body>
->>>>>>> 87a202466cb5060f174d09b379fbf151109ad0ac
     </html>
   );
 }
